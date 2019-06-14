116d95f874380d27b4632c70e2fcd380e8d0d569
Javi Domenech <javdome@gmail.com>
2019-06-14 16:28:40 +0200

    Maybe done ??

--- arrow.js
+++ arrow.js
@@ -1,7 +1,7 @@
 class Arrow {
 
-    constructor(svg, timeline) {
-        this.svg = svg;
+    constructor(timeline, dependencies) {
+        this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         this.timeline = timeline;
 
         this.arrowHead = document.createElementNS(
@@ -12,6 +12,12 @@ class Arrow {
             "http://www.w3.org/2000/svg",
             "path"
         );
+        
+        this.dependency = dependencies;
+
+        this.dependencyPath = [];
+
+        this.initialize();
     }
   
     initialize() {
@@ -39,7 +45,115 @@ class Arrow {
         this.arrowHeadPath.style.fill = "#F00";
         this.arrowHead.appendChild(this.arrowHeadPath);
         this.svg.appendChild(this.arrowHead);
+        //Create paths for the started dependency array
+        for (let i = 0; i < this.dependency.length; i++) {
+            this.createPath();
+        }
     }
     
+    createPath(){
+        //Add a new path to array dependencyPath and to svg
+        let somePath = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "path"
+          );
+          somePath.setAttribute("d", "M 0 0");
+          somePath.style.stroke = "#F00";
+          somePath.style.strokeWidth = "3px";
+          somePath.style.fill = "none";
+          this.dependencyPath.push(somePath);
+          this.svg.appendChild(somePath);
+    }
+
+    
+
+    drawDependencies() {
+        //Create paths for the started dependency array
+        for (let i = 0; i < this.dependency.length; i++) {
+            this.drawArrows(...this.dependency[i], i);
+        }
+        /*
+        this.dependency.map(function(dep, index) {
+           this.drawArrows(...dep, index);
+        });
+        */
+    }
+
+    drawArrows(i, j, index) {
+        //console.log(index);
+        //Checks if at least one item is visible in screen
+        var visibleItems = this.timeline.getVisibleItems();
+        var oneItemVisible = false;
+        for (let k = 0; k < visibleItems.length ; k++) {
+          if (i == visibleItems[k]) oneItemVisible = true;
+          if (j == visibleItems[k]) oneItemVisible = true;
+        }
+        //console.log(this.timeline);
+        //Checks if the groups of items are both visible
+        //let groupOf_i = items.get(i).group;
+        let groupOf_i = this.timeline.itemsData._data[i].group;
+        //let groupOf_j = items.get(j).group;
+        let groupOf_j = this.timeline.itemsData._data[j].group;
+        
+        if ( this.timeline.groupsData._data._data[groupOf_i].hasOwnProperty('visible') ) {
+            var groupOf_i_isVisible = this.timeline.groupsData._data._data[groupOf_i].visible;
+        } else {
+            var groupOf_i_isVisible = true;
+        }
+
+        if ( this.timeline.groupsData._data._data[groupOf_j].hasOwnProperty('visible') ) {
+            var groupOf_j_isVisible = this.timeline.groupsData._data._data[groupOf_j].visible;
+        } else {
+            var groupOf_j_isVisible = true;
+        }
+
+        if ( (groupOf_i_isVisible && groupOf_j_isVisible) && (oneItemVisible) ) {
+            var item_i = this.getItemPos(this.timeline.itemSet.items[i]);
+            var item_j = this.getItemPos(this.timeline.itemSet.items[j]);
+            if (item_j.mid_x < item_i.mid_x) [item_i, item_j] = [item_j, item_i]; // As demo, we put an arrow between item 0 and item1, from the one that is more on left to the one more on right.
+            var curveLen = item_i.height * 2; // Length of straight Bezier segment out of the item.
+            item_j.left -= 10; // Space for the arrowhead.
+            this.dependencyPath[index].setAttribute("marker-end", "url(#arrowhead0)");
+            this.dependencyPath[index].setAttribute(
+            "d",
+            "M " +
+                item_i.right +
+                " " +
+                item_i.mid_y +
+                " C " +
+                (item_i.right + curveLen) +
+                " " +
+                item_i.mid_y +
+                " " +
+                (item_j.left - curveLen) +
+                " " +
+                item_j.mid_y +
+                " " +
+                item_j.left +
+                " " +
+                item_j.mid_y
+            );
+        } else {
+            this.dependencyPath[index].setAttribute("marker-end", "");
+            this.dependencyPath[index].setAttribute("d", "M 0 0");
+        }
+
+    }
+
+    //Función que recibe in Item y devuelve la posición en pantalla del item.
+    getItemPos (item) {
+        let left_x = item.left;
+        let top_y = item.parent.top + item.parent.height - item.top - item.height;
+        return {
+            left: left_x,
+            top: top_y,
+            right: left_x + item.width,
+            bottom: top_y + item.height,
+            mid_x: left_x + item.width / 2,
+            mid_y: top_y + item.height / 2,
+            width: item.width,
+            height: item.height
+        }
+    }
   
   }
--- main.js
+++ main.js
@@ -7,9 +7,10 @@
         selectable: true,
         editable: true,
         onInitialDrawComplete: function() {
-          drawDependencies(dependency);
-          timelineplus.on("changed", () => {
-            drawDependencies(dependency);
+          myArrow.drawDependencies();
+          myArrow.timeline.on("changed", () => {
+            myArrow.drawDependencies();
+            
           }); //NOTE: We hijack the on "changed" event to draw the arrow.
         },
         groupTemplate: function(group) { //Con esto añadimos el boton de ocultar en los grupos
@@ -67,135 +68,18 @@
 
 
 
+
       /**
       *CREATING THE ARROWS 
       */    
 
+      //Dependencies array
+      var dependency = [[1, 2], [3, 5], [6, 7], [3, 8]];
+ 
+      // Create instance of Arrow for a timeline objetc and its denpedencies
+      const myArrow = new Arrow(timelineplus, dependency);
 
-      //Función que recibe in Item y devuelve la posición en pantalla del item.
-      const getItemPos = function(item) {
-      left_x = item.left;
-      top_y = item.parent.top + item.parent.height - item.top - item.height;
-      return {
-        left: left_x,
-        top: top_y,
-        right: left_x + item.width,
-        bottom: top_y + item.height,
-        mid_x: left_x + item.width / 2,
-        mid_y: top_y + item.height / 2,
-        width: item.width,
-        height: item.height
-      };
-      };
-
-      const drawArrows = function(i, j, index) {
-        //Checks if at least one item is visible in screen
-        var visibleItems = timelineplus.getVisibleItems();
-        var oneItemVisible = false;
-        for (let k = 0; k < visibleItems.length ; k++) {
-          if (i == visibleItems[k]) oneItemVisible = true;
-          if (j == visibleItems[k]) oneItemVisible = true;
-        }
-
-        //Checks if the groups of items are both visible
-        let groupOf_i = items.get(i).group;
-        let groupOf_j = items.get(j).group;
-        
-        if ( groups._data[groupOf_i].hasOwnProperty('visible') ) {
-            groupOf_i_isVisible = groups._data[groupOf_i].visible;
-        } else {
-            groupOf_i_isVisible = true;
-        }
-
-        if ( groups._data[groupOf_j].hasOwnProperty('visible') ) {
-            groupOf_j_isVisible = groups._data[groupOf_j].visible;
-        } else {
-            groupOf_j_isVisible = true;
-        }
-
-        if ( (groupOf_i_isVisible && groupOf_j_isVisible) && (oneItemVisible) ) {
-            var item_i = getItemPos(timelineplus.itemSet.items[i]);
-            var item_j = getItemPos(timelineplus.itemSet.items[j]);
-            if (item_j.mid_x < item_i.mid_x) [item_i, item_j] = [item_j, item_i]; // As demo, we put an arrow between item 0 and item1, from the one that is more on left to the one more on right.
-            var curveLen = item_i.height * 2; // Length of straight Bezier segment out of the item.
-            item_j.left -= 10; // Space for the arrowhead.
-            dependencyPath[index].setAttribute("marker-end", "url(#arrowhead0)");
-            dependencyPath[index].setAttribute(
-            "d",
-            "M " +
-                item_i.right +
-                " " +
-                item_i.mid_y +
-                " C " +
-                (item_i.right + curveLen) +
-                " " +
-                item_i.mid_y +
-                " " +
-                (item_j.left - curveLen) +
-                " " +
-                item_j.mid_y +
-                " " +
-                item_j.left +
-                " " +
-                item_j.mid_y
-            );
-        } else {
-            dependencyPath[index].setAttribute("marker-end", "");
-            dependencyPath[index].setAttribute("d", "M 0 0");
-        }
-
-      };
-
-      const dependency = [[1, 2], [3, 5], [6, 7], [3, 8]];
-      /*
-      const drawDependencies = dependency => {
-        dependency.map((dep, index) => drawArrows(...dep, index));
-      };
-      */
-     const drawDependencies = function(dependency) {
-      dependency.map(function(dep, index) {
-         drawArrows(...dep, index)
-        });
-      };
-
-
-
-
-
-
-
-
-      // Create SVG layer on top of timeline "center" div.
-      svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      const myArrow = new Arrow(svg, timelineplus);
-      myArrow.initialize();
       
-
-
-      // Add empty path (for now); it will be dynamically modified.
-      const dependencyPath = [];
-      //Aqui añadimos todas las flechas al DOM, pero de momento, todas tienen un "tamaño" de 0 (ver linea: somePath.setAttribute("d", "M 0 0");)
-      //Es decir, habra que para "pintarlas" hay que cambiar ese atributo.
-      //Ademas de al DOM, todos los elementos "flecha" tb se meten en el array dependencyPath.
-      for (let i = 0; i < dependency.length; i++) {
-        const somePath = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "path"
-        );
-        somePath.setAttribute("d", "M 0 0");
-        //somePath.setAttribute("marker-end", "url(#arrowhead0)");
-        somePath.style.stroke = "#F00";
-        somePath.style.strokeWidth = "3px";
-        somePath.style.fill = "none";
-        dependencyPath.push(somePath);
-        svg.appendChild(somePath);
-      }
-
-
-
-
-
-
 
 
 
